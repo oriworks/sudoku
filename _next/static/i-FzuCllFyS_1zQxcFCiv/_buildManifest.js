506e5cb70daaad34157dadcac567e13e20c42065
Joel Viegas <ext.joel.viegas@farfetch.com>
2021-12-29 23:03:13 +0000

    ci: deploy Next.js to gh-pages

---
+++ _next/static/i-FzuCllFyS_1zQxcFCiv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-3e95e207e4e1ce35.js","static/css/49a529820cf877f4.css","static/chunks/pages/index-370c80ee3fca25b2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
